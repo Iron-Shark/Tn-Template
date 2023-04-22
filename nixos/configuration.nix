5d117e4e46048d67f54c19654bfa1bb0b0bff1f4
Que <git@ironshark.org>
2023-04-22 16:49:40 -0400

    removed extra xserver config lines

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -52,9 +52,6 @@
   time.timeZone = "America/Detroit";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
   services.xserver = {
     layout = "us";
     xkbVariant = "";
