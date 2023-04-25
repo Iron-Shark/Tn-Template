12ba48aabb9896c956359a83bcedf4e5ece6f648
Que <git@ironshark.org>
2023-04-25 18:42:57 -0400

    fixed typo

--- SetUp/vortex-simple-2.sh
+++ SetUp/vortex-simple-2.sh
@@ -18,7 +18,7 @@ mkfs.btrfs -L root-$userName /dev/mapper/crypto-$userName
 echo "Creating Root Sub-volumes for user $userName"
 # replace this with mount /dev/disk/by-label/labelName /mnt
 # not sure how it will work but might make things a bit simpler
-mount -t btrfs /dev/nvme0n1/by-label/crypto-$userName /mnt
+mount -t btrfs /dev/nvme0n1/mapper/crypto-$userName /mnt
 btrfs subvolume create /mnt/etc
 btrfs subvolume create /mnt/log
 btrfs subvolume create /mnt/root
