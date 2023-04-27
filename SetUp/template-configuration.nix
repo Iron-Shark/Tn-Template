5067f60ce5b488dcb53ad406aa7ba05e99fc07ef
Que <git@ironshark.org>
2023-04-27 14:44:10 -0400

    created base for configuration template

---
+++ SetUp/template-configuration.nix
@@ -0,0 +1,96 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # This value determines the NixOS release from which the default settings for stateful data.
+  system.stateVersion = "22.05";
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "vortex"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Detroit";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Disable requiring SUDO password by users.
+  security.sudo.wheelNeedsPassword = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "colemak_dh";
+  };
+  services.xserver.xkbOptions = "caps:swapescape";
+  console.useXkbConfig = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pulseAudio.
+  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  sound.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    # Web Tools
+    firefox  # Must stay at system Level
+    gitFull
+    vim  # Must stay at system Level
+    pulseaudio
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    package = pkgs.nixFlakes;
+  };
+
+  environment.interactiveShellInit = ''
+    alias lx='ls -la'
+    alias logout='sudo kill -9 -1'
+  '';
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.que = {
+    isNormalUser = true;
+    description = "Que";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+
+}
