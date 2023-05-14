8234ea1b7a53924aef2d1aeff45c97733ef72512
Que <git@ironshark.org>
2023-05-14 16:22:13 -0400

    fixed missing opening line

--- home-manager/xin/home-apps/git.nix
+++ home-manager/xin/home-apps/git.nix
@@ -1,30 +1,33 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
 
-  enable = true;
-  package = pkgs.gitFull;
-  lfs.enable = true;
-  userName = "Que";
-  userEmail = "git@ironshark.org";
-  ignores = [
-    "*~"
-    ".*~"
-    "#*#"
-    "'#*#'"
-    ".*.swp"
-  ];
-  aliases = {
-    send = "! git status &&
+
+  programs.home-manager = {
+    enable = true;
+    package = pkgs.gitFull;
+    lfs.enable = true;
+    userName = "Que";
+    userEmail = "git@ironshark.org";
+    ignores = [
+      "*~"
+      ".*~"
+      "#*#"
+      "'#*#'"
+      ".*.swp"
+    ];
+    aliases = {
+      send = "! git status &&
 echo -n \"Commit Message: \" &&
 read -r commitMessage &&
 git add . &&
 git commit -m \"$commitMessage\" &&
 git push";
-  };
-  extraConfig = {
-    init = {
-      defaultBranch = "main";
-      pull = {
-        rebase = true;
+    };
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+        pull = {
+          rebase = true;
+        };
       };
     };
   };
