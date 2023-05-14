a04fd633e9deacbf053d708f635e4ab5f513b260
Que <git@ironshark.org>
2023-05-14 10:32:42 -0400

    commented out grimoire refs

--- home-manager/xin/emacs/init.el
+++ home-manager/xin/emacs/init.el
@@ -630,68 +630,68 @@
 )
 
 (defun Tn/org-find-time-file-property (property &optional anywhere)
-    "Return the position of the time file PROPERTY if it exists.
+  "Return the position of the time file PROPERTY if it exists.
 When ANYWHERE is non-nil, search beyond the preamble."
-    (save-excursion
-      (goto-char (point-min))
-      (let ((first-heading
-             (save-excursion
-               (re-search-forward org-outline-regexp-bol nil t))))
-        (when (re-search-forward (format "^#\\+%s:" property)
-                                 (if anywhere nil first-heading)
-                                 t)
-          (point)))))
+  (save-excursion
+    (goto-char (point-min))
+    (let ((first-heading
+           (save-excursion
+             (re-search-forward org-outline-regexp-bol nil t))))
+      (when (re-search-forward (format "^#\\+%s:" property)
+                               (if anywhere nil first-heading)
+                               t)
+        (point)))))
 
-  (defun Tn/org-has-time-file-property-p (property &optional anywhere)
-    "Return the position of time file PROPERTY if it is defined.
+(defun Tn/org-has-time-file-property-p (property &optional anywhere)
+  "Return the position of time file PROPERTY if it is defined.
 As a special case, return -1 if the time file PROPERTY exists but
 is not defined."
-    (when-let ((pos (Tn/org-find-time-file-property property anywhere)))
-      (save-excursion
-        (goto-char pos)
-        (if (and (looking-at-p " ")
-                 (progn (forward-char)
-                        (org-at-timestamp-p 'lax)))
-            pos
-          -1))))
+  (when-let ((pos (Tn/org-find-time-file-property property anywhere)))
+    (save-excursion
+      (goto-char pos)
+      (if (and (looking-at-p " ")
+               (progn (forward-char)
+                      (org-at-timestamp-p 'lax)))
+          pos
+        -1))))
 
-  (defun Tn/org-set-time-file-property (property &optional anywhere pos)
-    "Set the time file PROPERTY in the preamble.
+(defun Tn/org-set-time-file-property (property &optional anywhere pos)
+  "Set the time file PROPERTY in the preamble.
 When ANYWHERE is non-nil, search beyond the preamble.
 If the position of the file PROPERTY has already been computed,
 it can be passed in POS."
-    (when-let ((pos (or pos
-                        (Tn/org-find-time-file-property property))))
-      (save-excursion
-        (goto-char pos)
-        (if (looking-at-p " ")
-            (forward-char)
-          (insert " "))
-        (delete-region (point) (line-end-position))
-        (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
-          (insert now)))))
+  (when-let ((pos (or pos
+                      (Tn/org-find-time-file-property property))))
+    (save-excursion
+      (goto-char pos)
+      (if (looking-at-p " ")
+          (forward-char)
+        (insert " "))
+      (delete-region (point) (line-end-position))
+      (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
+        (insert now)))))
 
-  (defun Tn/org-set-last-modified ()
-    "Update the LAST_MODIFIED file property in the preamble."
-    (when (derived-mode-p 'org-mode)
-      (Tn/org-set-time-file-property "LAST_MODIFIED")))
+(defun Tn/org-set-last-modified ()
+  "Update the LAST_MODIFIED file property in the preamble."
+  (when (derived-mode-p 'org-mode)
+    (Tn/org-set-time-file-property "LAST_MODIFIED")))
 
-(setq org-capture-templates
-  '(("j" "Journal Entry"
-         entry (file+datetree "~/Grimoire/temp-journal.org")
-         "* %<%H:%M> %?"
-         :empty-lines 1)
-    ("f" "Food Log"
-         entry (file+datetree "~/Grimoire/temp-food-log.org")
-         "* %<%H:%M> %?"
-         :empty-lines 1)))
+;; (setq org-capture-templates
+;;   '(("j" "Journal Entry"
+;;          entry (file+datetree "~/Grimoire/temp-journal.org")
+;;          "* %<%H:%M> %?"
+;;          :empty-lines 1)
+;;     ("f" "Food Log"
+;;          entry (file+datetree "~/Grimoire/temp-food-log.org")
+;;          "* %<%H:%M> %?"
+;;          :empty-lines 1)))
 
 (add-hook 'org-capture-mode-hook 'evil-insert-state)
 
 (setq org-agenda-files (append
-                        (directory-files-recursively "~/Grimoire/" "\\.org$")
-                        (directory-files-recursively "~/Projects/" "\\.org$")
-                        ))
+                        ;; (directory-files-recursively "~/Grimoire/" "\\.org$")
+                        ;; (directory-files-recursively "~/Projects/" "\\.org$")
+                        ;; ))
 
 ;; (define-key org-agenda-mode-map "j" 'evil-next-line)
 ;; (define-key org-agenda-mode-map "k" 'evil-previous-line)
