5d605a1b708915a8c3bffb0fba353193e119eb74
Que <git@ironshark.org>
2023-05-14 08:33:07 -0400

    commented out straight.el

--- home-manager/xin/emacs/init.el
+++ home-manager/xin/emacs/init.el
@@ -16,32 +16,32 @@
 
 (setq package-enable-at-startup nil)
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; (defvar bootstrap-version)
+;; (let ((bootstrap-file
+;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+;;       (bootstrap-version 5))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
 
-(setq straight-repository-branch "develop")
+;; (setq straight-repository-branch "develop")
 
-(straight-use-package
- '(use-package
-    :type git
-    :host github
-    :repo "jwiegley/use-package"))
+;; (straight-use-package
+;;  '(use-package
+;;     :type git
+;;     :host github
+;;     :repo "jwiegley/use-package"))
 
-(eval-when-compile
-  (add-to-list 'load-path "~/.emacs.d/straight/build")
-  (require 'use-package))
+;; (eval-when-compile
+;;   (add-to-list 'load-path "~/.emacs.d/straight/build")
+;;   (require 'use-package))
 
-(setq straight-use-package-by-default t) ;Adds the straight argument to all use-package statements.
+;; (setq straight-use-package-by-default t) ;Adds the straight argument to all use-package statements.
 (setq use-package-always-ensure t) ;Adds the require argument to all use-package statements.
 
 (setq vc-follow-symlinks t)
